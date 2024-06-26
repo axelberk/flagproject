4d57f3928cf299d588d985968b33525f4d693141
Axel Bergquist <axelthorwaldbergquist@gmail.com>
2024-06-26 15:20:04 +0200

    styling for navbar

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import Navbar from "./Components/Navbar"
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <h1>Hello world</h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+import "./Navbar.css"
+import Brightness7SharpIcon from '@mui/icons-material/Brightness7Sharp';
+
+
+const Navbar = () => {
+    return (
+        <div className="Navbar">
+            <p>Världens Länder</p>
+            <img className="Logo" src="/logo.png" alt="" />
+            <button>
+                <Brightness7SharpIcon className="bright-icon"></Brightness7SharpIcon>Dark Mode
+                </button>
+        </div>
+    )
+}
+
+export default Navbar
