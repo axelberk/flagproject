84d294f0ee02ab883d2fc7acb69360e2e86810c6
Axel Bergquist <axelthorwaldbergquist@gmail.com>
2024-06-27 17:33:45 +0200

    dropdown styling

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ import Brightness7SharpIcon from '@mui/icons-material/Brightness7Sharp';
 const Navbar = () => {
     return (
         <div className="Navbar">
-            <p>Världens Länder</p>
+            <p>Countries of the World</p>
             <img className="Logo" src="/logo.png" alt="" />
             <button>
                 <Brightness7SharpIcon className="bright-icon"></Brightness7SharpIcon>Dark Mode
