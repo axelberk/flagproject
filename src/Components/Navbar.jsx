d3f01e85a87ba1906767269d36cb0bc90eaaec0e
Axel Bergquist <axelthorwaldbergquist@gmail.com>
2024-10-03 16:43:02 +0200

    made homepage clickable

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ import Brightness7SharpIcon from '@mui/icons-material/Brightness7Sharp';
 const Navbar = () => {
     return (
         <div className="Navbar">
-            <p>Countries of the World</p>
+            <a href="/" className="NavbarLink">Countries of the World</a>
             <img className="Logo" src="/logo.png" alt="" />
             <button>
                 <Brightness7SharpIcon className="bright-icon"></Brightness7SharpIcon>Dark Mode
