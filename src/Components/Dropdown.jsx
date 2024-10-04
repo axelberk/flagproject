0640d88997bb80572340229511c9c448d60f8496
Axel Bergquist <axelthorwaldbergquist@gmail.com>
2024-10-04 17:48:02 +0200

    changing to flexbox

--- src/Components/Dropdown.jsx
+++ src/Components/Dropdown.jsx
@@ -5,7 +5,6 @@ const Dropdown = () => {
         <div className="dropdownContainer">
             <label htmlFor="region" className="dropdownLabel">Region</label>
             <select id="region" className="Dropdown">
-                <option value="">Region</option>
                 <option value="all">All</option>
                 <option value="africa">Africa</option>
                 <option value="americas">Americas</option>
