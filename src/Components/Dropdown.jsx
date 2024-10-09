6b08603ec91f270e7b54e6ff58844c45f1acb22c
Axel Bergquist <axelthorwaldbergquist@gmail.com>
2024-10-09 15:48:51 +0200

    remaking dropdown cont

--- src/Components/Dropdown.jsx
+++ src/Components/Dropdown.jsx
@@ -2,18 +2,16 @@ import "./Dropdown.css"
 
 const Dropdown = () => {
     return (
-        <div className="dropdownContainer">
-            <select id="region" className="Dropdown" required>
-                <option value=""></option>
-                <option value="all">All</option>
-                <option value="africa">Africa</option>
-                <option value="americas">Americas</option>
-                <option value="asia">Asia</option>
-                <option value="europe">Europe</option>
-                <option value="oceania">Oceania</option>
-
-            </select>
-            <label htmlFor="region" className="dropdownLabel">Region</label>
+        <div className="customDropdown">
+            <div className="selectedOption">Region</div>
+            <div className="dropdownOptions">
+                <div className="option">All</div>
+                <div className="option">Africa</div>
+                <div className="option">Americas</div>
+                <div className="option">Asia</div>
+                <div className="option">Europe</div>
+                <div className="option">Oceania</div>
+            </div>
         </div>
         
     )
