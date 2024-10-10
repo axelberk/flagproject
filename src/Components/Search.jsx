834a795aa65afc932df27daed1689f4c309d1436
Axel Bergquist <axelthorwaldbergquist@gmail.com>
2024-10-10 15:25:15 +0200

    remaking search bar with mui

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,4 +1,28 @@
-import "./Search.css"
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+
+export default function Search() {
+  return (
+    <Box
+      component="form"
+      sx={{ '& > :not(style)': { m: 1, width: '400px', border: '1px solid #f2f2f288',
+        '& MuiOutlinedInput-root' : {
+            '&fieldset': {
+                borderColor: '#f2f2f2'
+                
+            }
+        }
+       } }}
+      noValidate
+      autoComplete="off"
+    >
+      <TextField id="outlined-basic" label="Search for a country..." variant="outlined" />
+    </Box>
+  );
+}
+
+/* import "./Search.css"
 
 const Search = () => {
     return (
@@ -11,4 +35,4 @@ const Search = () => {
     )
 }
 
-export default Search
+export default Search */
