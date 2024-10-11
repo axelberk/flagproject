cc94cf5846e1de7b91e932504b4fc57d8042c434
Axel Bergquist <axelthorwaldbergquist@gmail.com>
2024-10-11 20:39:28 +0200

    sorted countries by alpahetical

--- src/Components/CountryCard.jsx
+++ src/Components/CountryCard.jsx
@@ -1,33 +1,35 @@
-import React, { useState, useEffect } from "react"
-import "./CountryCard.css"
+import React, { useState, useEffect } from "react";
 
 function CountryCard() {
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
 
-    useEffect(() => {
-        fetch("https://restcountries.com/v3.1/all")
-        .then(response => response.json())
-        .then(json => setData(json))
-        .catch(error => console.error(error))
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((response) => response.json())
+      .then((json) => {
+        const sortedCountries = json.sort((a, b) => {
+          return a.name.common.localeCompare(b.name.common);
+        });
+        setData(sortedCountries);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
-    }, [])
-
-    return (
-        <div className="cards-grid">
-            {data.map((country) => (
-                <div className="card" key={country.cca3}>
-                    <img src={country.flags.svg} alt={`${country.name.common} flag`} className="flag" />
-                    <div className="card-info">
-                        <h3>{country.name.common}</h3>
-                        <p><strong>Population:</strong> {country.population.toLocaleString()}</p>
-                        <p><strong>Region:</strong> {country.region}</p>
-                        <p><strong>Capital:</strong> {country.capital ? country.capital[0]: "None"}</p>
-                    </div>
-                </div>
-            ))}
+  return (
+    <div className="cards-grid">
+      {data.map((country) => (
+        <div className="card" key={country.cca3}>
+          <img src={country.flags.svg} alt={`${country.name.common} flag`} className="flag" />
+          <div className="card-content">
+            <h3>{country.name.common}</h3>
+            <p><strong>Population:</strong> {country.population.toLocaleString()}</p>
+            <p><strong>Region:</strong> {country.region}</p>
+            <p><strong>Capital:</strong> {country.capital ? country.capital[0] : "N/A"}</p>
+          </div>
         </div>
-    )
-    
+      ))}
+    </div>
+  );
 }
 
-export default CountryCard
+export default CountryCard;
