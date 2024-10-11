3596752d313e2189d3bc5a180d4bafb479fa8097
Axel Bergquist <axelthorwaldbergquist@gmail.com>
2024-10-11 21:06:05 +0200

    re-did styling for country cards

--- src/Components/CountryCard.jsx
+++ src/Components/CountryCard.jsx
@@ -20,7 +20,7 @@ function CountryCard() {
       {data.map((country) => (
         <div className="card" key={country.cca3}>
           <img src={country.flags.svg} alt={`${country.name.common} flag`} className="flag" />
-          <div className="card-content">
+          <div className="card-info">
             <h3>{country.name.common}</h3>
             <p><strong>Population:</strong> {country.population.toLocaleString()}</p>
             <p><strong>Region:</strong> {country.region}</p>
