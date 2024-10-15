4dcea988a4ee24845d0bc2028f612a69831266a8
Axel Bergquist <axelthorwaldbergquist@gmail.com>
2024-10-15 19:38:02 +0200

    countrypage styling, tbc

--- src/Components/CountryPage.jsx
+++ src/Components/CountryPage.jsx
@@ -4,14 +4,14 @@ import { useParams, Link } from "react-router-dom";
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 const CountryPage = () => {
-  const { countryCode } = useParams();
+  const { countrycode } = useParams();
   const [countryData, setCountryData] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchCountryData = async () => {
       try {
-        const response = await fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`);
+        const response = await fetch(`https://restcountries.com/v3.1/alpha/${countrycode}`);
         const data = await response.json();
         setCountryData(data[0]);
         setLoading(false);
@@ -21,7 +21,7 @@ const CountryPage = () => {
       }
     };
     fetchCountryData();
-  }, [countryCode]);
+  }, [countrycode]);
 
   if (loading) {
     return <div>Loading...</div>;
@@ -41,16 +41,22 @@ const CountryPage = () => {
                         </button>
                 </Link>
             </div>
-            <img src={country.flags.svg} alt={`${countryData.name.common} flag`} className="country-flag"/>
+            <img src={countryData.flags.svg} alt={`${countryData.name.common} flag`} className="country-flag"/>
             <div className="country-details">
                 <h3>{countryData.name.common}</h3>
-                <p><strong>Population: </strong>{countryData.population.toLocaleString()}</p>
-                <p><strong>Region: </strong>{countryData.region}</p>
-                <p><strong>Capital: </strong>{countryData.capital?.[0]}</p>
-                <p><strong>Native name: </strong>{Object.values(countryData.name.nativeName)?.[0]?.common}</p>
-                <p><strong>Top Level Domain: </strong>{countryData.tld?.join(', ')}</p>
-                <p><strong>Currencies: </strong>{Object.values(countryData.currencies)?.[0]?.name}</p>
-                <p><strong>Language: </strong>{Object.values(countryData.languages)?.join(', ')}</p>
+                <div className="facts-container">
+                    <div className="facts-one">
+                        <p><strong>Population: </strong>{countryData.population.toLocaleString()}</p>
+                        <p><strong>Region: </strong>{countryData.region}</p>
+                        <p><strong>Capital: </strong>{countryData.capital?.[0]}</p>
+                        <p><strong>Native name: </strong>{Object.values(countryData.name.nativeName)?.[0]?.common}</p>
+                    </div>
+                    <div className="facts-two">
+                        <p><strong>Top Level Domain: </strong>{countryData.tld?.join(', ')}</p>
+                        <p><strong>Currencies: </strong>{Object.values(countryData.currencies)?.[0]?.name}</p>
+                        <p><strong>Language: </strong>{Object.values(countryData.languages)?.join(', ')}</p>
+                </div>
+                </div>
                 <div className="border-countries">
                     <p><strong>Border Countries: </strong></p>
                     {countryData.borders?.length ? (
@@ -62,6 +68,7 @@ const CountryPage = () => {
                     ) : (
                         <p>None</p>
                     )}
+                    
                 </div>
             </div>
         </div>
