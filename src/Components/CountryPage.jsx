433c01c4c2cde00be14989eb068585474ce979ff
Axel Bergquist <axelthorwaldbergquist@gmail.com>
2024-10-16 19:38:45 +0200

    countrypage styling cont

--- src/Components/CountryPage.jsx
+++ src/Components/CountryPage.jsx
@@ -31,48 +31,50 @@ const CountryPage = () => {
     return <div>Country not found</div>;
   }
 
-    return (
-        <div className="CountryPage">
-            <div className="back-section">
-                <Link to="/">
-                    <button className="back-button">
-                        <ArrowBackIcon></ArrowBackIcon>
-                        Back
-                        </button>
-                </Link>
-            </div>
-            <img src={countryData.flags.svg} alt={`${countryData.name.common} flag`} className="country-flag"/>
-            <div className="country-details">
-                <h3>{countryData.name.common}</h3>
-                <div className="facts-container">
-                    <div className="facts-one">
-                        <p><strong>Population: </strong>{countryData.population.toLocaleString()}</p>
-                        <p><strong>Region: </strong>{countryData.region}</p>
-                        <p><strong>Capital: </strong>{countryData.capital?.[0]}</p>
-                        <p><strong>Native name: </strong>{Object.values(countryData.name.nativeName)?.[0]?.common}</p>
-                    </div>
-                    <div className="facts-two">
-                        <p><strong>Top Level Domain: </strong>{countryData.tld?.join(', ')}</p>
-                        <p><strong>Currencies: </strong>{Object.values(countryData.currencies)?.[0]?.name}</p>
-                        <p><strong>Language: </strong>{Object.values(countryData.languages)?.join(', ')}</p>
-                </div>
-                </div>
-                <div className="border-countries">
-                    <p><strong>Border Countries: </strong></p>
-                    {countryData.borders?.length ? (
-                        countryData.borders.map((border) => (
-                            <Link key={border} to={`/country/${border}`} className="border-country">
-                                {border}
-                            </Link>
-                        ))
-                    ) : (
-                        <p>None</p>
-                    )}
-                    
-                </div>
-            </div>
-        </div>
-    )    
-}
+  return (
+    <div className="CountryPage">
+  <div className="left-section">
+    <div className="back-section">
+      <Link to="/">
+        <button className="back-button">
+          <ArrowBackIcon></ArrowBackIcon>
+          Back
+        </button>
+      </Link>
+    </div>
+    <img src={countryData.flags.svg} alt={`${countryData.name.common} flag`} className="country-flag"/>
+  </div>
+  <div className="country-details">
+    <h3>{countryData.name.common}</h3>
+    <div className="facts-container">
+      <div className="facts-one">
+        <p><strong>Population: </strong>{countryData.population.toLocaleString()}</p>
+        <p><strong>Region: </strong>{countryData.region}</p>
+        <p><strong>Capital: </strong>{countryData.capital?.[0]}</p>
+        <p><strong>Native name: </strong>{Object.values(countryData.name.nativeName)?.[0]?.common}</p>
+      </div>
+      <div className="facts-two">
+        <p><strong>Top Level Domain: </strong>{countryData.tld?.join(', ')}</p>
+        <p><strong>Currencies: </strong>{Object.values(countryData.currencies)?.[0]?.name}</p>
+        <p><strong>Language: </strong>{Object.values(countryData.languages)?.join(', ')}</p>
+      </div>
+    </div>
+    <div className="border-countries">
+      <p><strong>Border Countries: </strong></p>
+      {countryData.borders?.length ? (
+        countryData.borders.map((border) => (
+          <Link key={border} to={`/country/${border}`} className="border-country">
+            {border}
+          </Link>
+        ))
+      ) : (
+        <p>None</p>
+      )}
+    </div>
+  </div>
+</div>
 
-export default CountryPage
+  );
+};
+
+export default CountryPage;
